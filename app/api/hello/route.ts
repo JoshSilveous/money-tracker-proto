3df08ce4ea6cf97111c46266247c5c2d20a6f5b0
Joshua Silveous <JoshSilveous@gmail.com>
2024-04-20 12:56:26 -0400

    removed unnecessary files

--- app/api/hello/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-	return Response.json({ message: 'Hello from Next.js!' })
-}
-export async function POST(req: Request) {
-	const data = await req.json()
-	console.log(data)
-	return Response.json({ message: 'Received!' })
-}
