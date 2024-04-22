b37d92da864b7c3f8903fca7d72a466433cfb305
Joshua Silveous <JoshSilveous@gmail.com>
2024-04-22 14:30:00 -0400

    added method for getting a book

--- app/api/authenticated/db/books/get_one/route.ts
+++ app/api/authenticated/db/books/get_one/route.ts
@@ -1,25 +1,32 @@
-import { NewBook, bookInsert, newBookSchema } from '@/app/data/books'
+import { bookGetOne } from '@/app/data/books/get'
+import { parseAndValidate } from '@/app/util/parseAndValidate'
 import { createToken } from '@/app/util/token/token'
+import Joi from 'joi'
 
-export async function GET(req: Request) {
-	const data = await req.json()
-	const userUUID = req.headers.get('authenticated-uuid')!
-
-	const { error: joiError } = newBookSchema.validate(data.payload)
-	if (joiError) {
-		return Response.json(joiError, { status: 401, statusText: 'Invalid Data' })
+export async function POST(req: Request) {
+	const { error, data } = await parseAndValidate(
+		req,
+		Joi.object({
+			payload: Joi.object({ book_uuid: Joi.string().required() }).required(),
+		})
+	)
+	if (error) {
+		return Response.json({ message: error }, { status: 401 })
 	}
+
+	const userUUID = req.headers.get('authenticated-uuid')!
 	const newToken = await createToken(userUUID)
 
 	try {
-		const payload = data.payload as NewBook
-		const res = await bookInsert(userUUID, payload)
+		const bookUUID = data.payload.book_uuid
+		const res = await bookGetOne(userUUID, bookUUID)
 		return Response.json({
 			message: `Success`,
 			book: res.rows[0],
 			newToken: newToken,
 		})
 	} catch (e) {
+		console.log(e)
 		return Response.json({ message: 'Internal Server Error' }, { status: 500 })
 	}
 }
